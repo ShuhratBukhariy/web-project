ef58ff6e43667c921298511b8158c26264473654
Shuhrat Bukhariy <102745438+ShuhratBukhariy@users.noreply.github.com>
2023-02-13 17:57:18 +0500

    shuhrat

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service', '@volar-plugins/vetur')
 module.exports = defineConfig({
   transpileDependencies: true
 })
